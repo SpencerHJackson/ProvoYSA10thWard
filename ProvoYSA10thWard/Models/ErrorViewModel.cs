2d0010b0a654c3e9d9eb767bd722b64ef4a8be10
SpencerHJackson <90439665+SpencerHJackson@users.noreply.github.com>
2023-01-18 21:26:40 -0700

    Add project files.

---
+++ ProvoYSA10thWard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProvoYSA10thWard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
