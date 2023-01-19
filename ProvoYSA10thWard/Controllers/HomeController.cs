f8057356ade917bdef6028a485c3c531dde5d7b1
SpencerHJackson <90439665+SpencerHJackson@users.noreply.github.com>
2023-01-18 21:52:43 -0700

    Add FHE and Ward Prayer pages

--- ProvoYSA10thWard/Controllers/HomeController.cs
+++ ProvoYSA10thWard/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace ProvoYSA10thWard.Controllers
             return View();
         }
 
+        public IActionResult FamilyHomeEvening()
+        {
+            return View();
+        }
+
+        public IActionResult WardPrayer()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
