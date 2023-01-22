558f503e1e739d1ca0ddf539b4dfcbcb3e91fc2e
SpencerHJackson <90439665+SpencerHJackson@users.noreply.github.com>
2023-01-22 15:30:44 -0700

    Add munch and mingle page

--- ProvoYSA10thWard/Controllers/HomeController.cs
+++ ProvoYSA10thWard/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace ProvoYSA10thWard.Controllers
             return View();
         }
 
+        public IActionResult MunchAndMingle()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
